9396c162b973545042bfcc075421d9958d45953d
Tyler Hancock <darklime@live.ca>
2024-11-07 18:57:27 -0700

    Add creative tab entry. Fixes item not displaying in mods like JEI.

--- common/src/main/java/net/darkhax/waybackstone/Content.java
+++ common/src/main/java/net/darkhax/waybackstone/Content.java
@@ -1,12 +1,16 @@
 package net.darkhax.waybackstone;
 
+import net.darkhax.bookshelf.api.function.CachedSupplier;
 import net.darkhax.bookshelf.api.registry.RegistryDataProvider;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 
 public class Content extends RegistryDataProvider {
 
     public Content() {
 
         super(WayBackStoneMod.MOD_ID);
+        this.withItemTab(CachedSupplier.cache(() -> BuiltInRegistries.ITEM.get(ResourceLocation.tryBuild(WayBackStoneMod.MOD_ID, "way_back_stone")).getDefaultInstance()));
         this.items.add(WayBackStoneItem::new, "way_back_stone");
     }
 }
